6aabf58eefd4b3abb6f6c053fe5d48afaa8040a8
Javier Guerra Melgares <javier.guerra@dekra.com>
2019-10-15 14:30:00 +0200

    Add architecure as a parameter to build the OTGC

--- build/debian/template_control.sh
+++ build/debian/template_control.sh
@@ -1,9 +1,17 @@
+#!/bin/bash
+
+# Define parameters which are passed in
+VERSION=$1
+ARCH=$2
+
+# Define the template
+cat << EOF
 Package: OTGC
-Version: 2.1.0
+Version: $VERSION
 Section: custom
 Priority: optional
-Architecture: amd64
+Architecture: $ARCH
 Pre-Depends: openjdk-8-jdk, openjfx
 Maintainer: DEKRA Testing and Certification, S.A.U.
 Description: Onboarding Tool and Generic Client.
-
+EOF
