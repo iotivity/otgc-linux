cb04b7fd82d8629aa0a5476a43c354246b9cfc13
Wouter <wavdb66@gmail.com>
2021-06-21 11:48:52 +0100

    more seperation of scripts

--- build_iotivity.sh
+++ build_iotivity.sh
@@ -15,13 +15,6 @@ mkdir jni
 cd ..
 cd ..
 
-# build the lib 
-# clone in top_level
-rm -rf iotivity-lite
-git clone https://github.com/iotivity/iotivity-lite.git
-cd iotivity-lite
-#git checkout bb53715d5e4dbe30360685690bac61f2e4546f6b
-git checkout master
 
 cd ./port/linux
 make DEBUG=1 SECURE=1 IPV4=1 TCP=1 PKI=1 DYNAMIC=1 CLOUD=1 JAVA=1 IDD=1 
--- setup.sh
+++ setup.sh
@@ -10,6 +10,8 @@ cd otgc-linux
 # install the tools
 sh ./install_tools.sh
 # build the iotivity library
+sh ./checkout_iotivity.sh
+# build the iotivity library
 sh ./build_iotivity.sh
 # build the linux otgc application (including .deb file)
 sh ./build_otgc.sh
